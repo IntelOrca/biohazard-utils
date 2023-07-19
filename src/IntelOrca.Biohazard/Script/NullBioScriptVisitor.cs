8d6a4430a02dfa95d0c98c2ec4de9f9f71bef6f2
Ted John <ted@brambles.org>
2023-07-19 22:08:49 +0100

    Improve SCD length detection

---
+++ src/IntelOrca.Biohazard/Script/NullBioScriptVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IntelOrca.Biohazard.Script
+{
+    public sealed class NullBioScriptVisitor : IBioScriptVisitor
+    {
+        public void VisitVersion(BioVersion version) { }
+        public void VisitBeginScript(BioScriptKind kind) { }
+        public void VisitBeginSubroutine(int index) { }
+        public void VisitOpcode(int offset, Span<byte> opcodeBytes) { }
+        public void VisitEndSubroutine(int index) { }
+        public void VisitEndScript(BioScriptKind kind) { }
+        public void VisitTrailingData(int offset, Span<byte> data) { }
+    }
+}

0660f792cf90204bf52732f687a80ea48773e1ed
Ted John <ted@brambles.org>
2024-03-02 00:54:59 +0000

    Add initial TIM2 code

--- src/IntelOrca.Biohazard/Room/CvTextureList.cs
+++ src/IntelOrca.Biohazard/Room/CvTextureList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using IntelOrca.Biohazard.Extensions;
 
 namespace IntelOrca.Biohazard.Room
@@ -17,6 +18,33 @@ namespace IntelOrca.Biohazard.Room
 
         public int Count => Data.GetSafeSpan<int>(0, 1)[0];
         public ReadOnlySpan<int> Offsets => Data.GetSafeSpan<int>(4, Count);
+
+        public ReadOnlySpan<Tim2> Tims
+        {
+            get
+            {
+                var result = new Tim2[Count];
+                for (var i = 0; i < result.Length; i++)
+                {
+                    result[i] = GetTim(i);
+                }
+                return result;
+            }
+        }
+
+        private Tim2 GetTim(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            var offsets = Offsets;
+            var offset = offsets[index] - BaseOffset;
+            var endOffset = index < Count - 1 ? offsets[index + 1] - BaseOffset : Data.Length;
+            var timData = Data[offset..endOffset];
+            var actualTimLength = MemoryMarshal.Cast<byte, int>(timData.Span.Slice(4, 4))[0];
+            var actualTimData = timData.Slice(32, actualTimLength);
+            return new Tim2(actualTimData);
+        }
 
         public CvTextureList WithNewBaseOffset(int baseOffset)
         {
--- src/IntelOrca.Biohazard/Tim2.cs
+++ src/IntelOrca.Biohazard/Tim2.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using IntelOrca.Biohazard.Extensions;
 
 namespace IntelOrca.Biohazard
 {
@@ -9,6 +11,62 @@ namespace IntelOrca.Biohazard
         public Tim2(ReadOnlyMemory<byte> data)
         {
             Data = data;
+        }
+
+        public int Magic => Data.GetSafeSpan<int>(0, 1)[0];
+        public byte Revision => Data.Span[4];
+        public byte Format => Data.Span[5];
+        public short PictureCount => Data.GetSafeSpan<short>(6, 1)[0];
+
+        private int GetPictureOffset(int index)
+        {
+            var headerEnd = Format > 0 ? 0x80 : 0x10;
+            return headerEnd;
+        }
+
+        public PictureHeader GetHeader(int index)
+        {
+            var offset = GetPictureOffset(index);
+            var header = MemoryMarshal.Cast<byte, PictureHeader>(Data.Span[offset..])[0];
+            return header;
+        }
+
+        public ReadOnlySpan<byte> GetPixelData(int index)
+        {
+            var offset = GetPictureOffset(index);
+            var header = GetHeader(index);
+            var pixelOffset = offset + header.HeaderSize;
+            var pixelLength = header.ImageSize;
+            return Data.Span.Slice(pixelOffset, pixelLength);
+        }
+
+        public ReadOnlySpan<byte> GetPaletteData(int index)
+        {
+            var offset = GetPictureOffset(index);
+            var header = GetHeader(index);
+            var paletteOffset = offset + header.HeaderSize + header.ImageSize;
+            var paletteLength = header.ClutSize;
+            return Data.Span.Slice(paletteOffset, paletteLength);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PictureHeader
+        {
+            public int TotalSize;
+            public int ClutSize;
+            public int ImageSize;
+            public short HeaderSize;
+            public short ColoursPerClut;
+            public byte Format;
+            public byte MipmapCount;
+            public byte ClutColourType;
+            public byte ImageColourType;
+            public short Width;
+            public short Height;
+            public long GsTex0;
+            public long GsTex1;
+            public int GsFlags;
+            public int GsClut;
         }
     }
 }

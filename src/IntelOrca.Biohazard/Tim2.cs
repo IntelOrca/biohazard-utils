031e030d2ee44549c8e9a1f2d42332c126b4d97b
Ted John <ted@brambles.org>
2024-03-05 23:05:30 +0000

    Implement TIM2 import

--- src/IntelOrca.Biohazard/Tim2.cs
+++ src/IntelOrca.Biohazard/Tim2.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.InteropServices;
 using IntelOrca.Biohazard.Extensions;
 
@@ -17,11 +19,21 @@ namespace IntelOrca.Biohazard
         public byte Revision => Data.Span[4];
         public byte Format => Data.Span[5];
         public short PictureCount => Data.GetSafeSpan<short>(6, 1)[0];
+        public uint Reserved1 => Data.GetSafeSpan<uint>(8, 1)[0];
+        public uint Reserved2 => Data.GetSafeSpan<uint>(12, 1)[0];
 
         private int GetPictureOffset(int index)
         {
             var headerEnd = Format > 0 ? 0x80 : 0x10;
             return headerEnd;
+        }
+
+        public Picture GetPicture(int index)
+        {
+            var offset = GetPictureOffset(index);
+            var header = MemoryMarshal.Cast<byte, PictureHeader>(Data.Span[offset..])[0];
+            var totalSize = header.TotalSize;
+            return new Picture(Data.Slice(offset, totalSize));
         }
 
         public PictureHeader GetHeader(int index)
@@ -49,10 +61,46 @@ namespace IntelOrca.Biohazard
             return Data.Slice(paletteOffset, paletteLength);
         }
 
-        public Picture Picture0 => new Picture(
-            GetHeader(0),
-            GetPaletteData(0),
-            GetPixelData(0));
+        public Picture Picture0 => GetPicture(0);
+
+        public Builder ToBuilder()
+        {
+            var builder = new Builder();
+            builder.Reserved1 = Reserved1;
+            builder.Reserved2 = Reserved2;
+            for (var i = 0; i < PictureCount; i++)
+            {
+                builder.Pictures.Add(GetPicture(i));
+            }
+            return builder;
+        }
+
+        public class Builder
+        {
+            public uint Reserved1 { get; set; }
+            public uint Reserved2 { get; set; }
+            public List<Picture> Pictures { get; } = new List<Picture>();
+
+            public Tim2 ToTim2()
+            {
+                var ms = new MemoryStream();
+                var bw = new BinaryWriter(ms);
+
+                bw.Write(0x324D4954);
+                bw.Write((byte)4);
+                bw.Write((byte)1);
+                bw.Write((ushort)Pictures.Count);
+                bw.Write(Reserved1);
+                bw.Write(Reserved2);
+
+                bw.Seek(0x80, SeekOrigin.Begin);
+                foreach (var picture in Pictures)
+                {
+                    bw.Write(picture.Data);
+                }
+                return new Tim2(ms.ToArray());
+            }
+        }
 
         [StructLayout(LayoutKind.Sequential)]
         public struct PictureHeader
@@ -76,35 +124,39 @@ namespace IntelOrca.Biohazard
 
         public readonly struct Picture
         {
+            public ReadOnlyMemory<byte> Data { get; }
+
             private readonly PictureHeader _header;
+            private readonly ReadOnlyMemory<byte> _paletteData;
+            private readonly ReadOnlyMemory<byte> _pixelData;
+            private readonly ReadOnlyMemory<byte> _fixedPalette;
 
-            public PictureHeader Header => _header;
-            public ReadOnlyMemory<byte> PaletteData { get; }
-            public ReadOnlyMemory<byte> PixelData { get; }
-
-            public Picture(PictureHeader header, ReadOnlyMemory<byte> paletteData, ReadOnlyMemory<byte> pixelData)
+            public Picture(ReadOnlyMemory<byte> data)
             {
-                _header = header;
-                PaletteData =
-                    header.ImageColourType == 5 ?
-                        FixPalette(paletteData) :
-                        paletteData;
-                PixelData = pixelData;
+                Data = data;
+
+                _header = MemoryMarshal.Cast<byte, PictureHeader>(Data.Span)[0];
+                _paletteData = Data.Slice(_header.HeaderSize + _header.ImageSize, _header.ClutSize);
+                _pixelData = Data.Slice(_header.HeaderSize, _header.ImageSize);
+                _fixedPalette = _header.ImageColourType == 5 ?
+                    FixPalette(_paletteData.Span) :
+                    _paletteData;
             }
 
+            public PictureHeader Header => _header;
             public int Width => _header.Width;
             public int Height => _header.Height;
 
             public int GetColour(int paletteIndex)
             {
-                var palette = MemoryMarshal.Cast<byte, int>(PaletteData.Span);
+                var palette = MemoryMarshal.Cast<byte, int>(_fixedPalette.Span);
                 return palette[paletteIndex];
             }
 
             public byte GetPixelRaw(int x, int y)
             {
                 ref readonly var header = ref _header;
-                var pixelData = PixelData.Span;
+                var pixelData = _pixelData.Span;
                 if (header.ImageColourType == 5)
                 {
                     return pixelData[y * header.Width + x];
@@ -126,9 +178,9 @@ namespace IntelOrca.Biohazard
                 return Rgba2Argb(GetColour(raw));
             }
 
-            private static ReadOnlyMemory<byte> FixPalette(ReadOnlyMemory<byte> paletteData)
+            private static ReadOnlyMemory<byte> FixPalette(ReadOnlySpan<byte> paletteData)
             {
-                var colours = MemoryMarshal.Cast<byte, int>(paletteData.Span);
+                var colours = MemoryMarshal.Cast<byte, int>(paletteData);
                 var resultData = new byte[colours.Length * 4];
                 var result = MemoryMarshal.Cast<byte, int>(resultData);
                 var parts = colours.Length / 32;
@@ -160,6 +212,137 @@ namespace IntelOrca.Biohazard
                 int b = (value >> 16) & 0xFF;
                 int a = (value >> 24) & 0xFF;
                 return (a << 24) | (r << 16) | (g << 8) | b;
+            }
+
+            private static int Argb2Rgba(int value)
+            {
+                int a = (value >> 24) & 0xFF;
+                int r = (value >> 16) & 0xFF;
+                int g = (value >> 8) & 0xFF;
+                int b = (value & 0xFF);
+                return (a << 24) | (b << 16) | (g << 8) | r;
+            }
+
+            public byte[] ToBitmapBuffer()
+            {
+                var stride = (((Width * 3) + 3) / 4) * 4;
+                var padding = stride - (Width * 3);
+                var rawDataLength = stride * Height;
+
+                var ms = new MemoryStream();
+                var bw = new BinaryWriter(ms);
+
+                // BMP header
+                bw.Write((byte)0x42);
+                bw.Write((byte)0x4D);
+                bw.Write(0);
+                bw.Write((ushort)0);
+                bw.Write((ushort)0);
+                bw.Write((uint)54);
+
+                // DIB header
+                bw.Write((uint)40);
+                bw.Write((uint)Width);
+                bw.Write((uint)Height);
+                bw.Write((ushort)1);
+                bw.Write((ushort)24);
+                bw.Write((uint)0);
+                bw.Write((uint)rawDataLength);
+                bw.Write((uint)0x0B13); // DPI X
+                bw.Write((uint)0x0B13); // DPI Y
+                bw.Write((uint)0);
+                bw.Write((uint)0);
+
+                for (var y = 0; y < Height; y++)
+                {
+                    for (var x = 0; x < Width; x++)
+                    {
+                        var p = GetPixel(x, Height - y - 1);
+                        bw.Write((byte)((p >> 0) & 0xFF));
+                        bw.Write((byte)((p >> 8) & 0xFF));
+                        bw.Write((byte)((p >> 16) & 0xFF));
+                    }
+                    for (var i = 0; i < padding; i++)
+                    {
+                        bw.Write(0);
+                    }
+                }
+
+                ms.Position = 2;
+                bw.Write((uint)ms.Length);
+
+                return ms.ToArray();
+            }
+
+            public Builder ToBuilder()
+            {
+                return new Builder(_header, _paletteData.ToArray(), _pixelData.ToArray());
+            }
+
+            public class Builder
+            {
+                public PictureHeader Header { get; }
+                public int Width => Header.Width;
+                public int Height => Header.Height;
+                public byte[] Palette { get; }
+                public byte[] PixelData { get; }
+
+                public Builder(PictureHeader header, byte[] palette, byte[] pixelData)
+                {
+                    Header = header;
+                    Palette = palette;
+                    PixelData = pixelData;
+                }
+
+                public void Import(uint[] argb)
+                {
+                    var paletteSize = 0;
+                    var colours = new Dictionary<int, byte>();
+                    var unusedColours = new HashSet<int>();
+                    var pixelData = new byte[argb.Length];
+                    for (var i = 0; i < argb.Length; i++)
+                    {
+                        var colour = Argb2Rgba((int)argb[i]);
+                        if (!colours.TryGetValue(colour, out var paletteIndex))
+                        {
+                            if (paletteSize >= 256)
+                            {
+                                // Ran out of colours
+                                paletteIndex = 0;
+                                unusedColours.Add(colour);
+                            }
+                            else
+                            {
+                                paletteIndex = (byte)paletteSize;
+                                paletteSize++;
+                                colours.Add(colour, paletteIndex);
+                            }
+                        }
+                        pixelData[i] = paletteIndex;
+                    }
+
+                    var palette = new int[paletteSize];
+                    foreach (var kvp in colours)
+                    {
+                        palette[kvp.Value] = kvp.Key;
+                    }
+                    var paletteData = MemoryMarshal.Cast<int, byte>(palette);
+                    var paletteData2 = FixPalette(paletteData).ToArray();
+
+                    paletteData2.CopyTo(new Span<byte>(Palette));
+                    pixelData.CopyTo(PixelData, 0);
+                }
+
+                public Picture ToPicture()
+                {
+                    var ms = new MemoryStream();
+                    var bw = new BinaryWriter(ms);
+                    bw.Write(Header);
+                    bw.Seek(Header.HeaderSize, SeekOrigin.Begin);
+                    bw.Write(PixelData);
+                    bw.Write(Palette);
+                    return new Picture(ms.ToArray());
+                }
             }
         }
     }
